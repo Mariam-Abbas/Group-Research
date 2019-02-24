15e4df63b112cfdb22649103a416f7f9fc91d7fc
Mariam-Abbas <32878145+Mariam-Abbas@users.noreply.github.com>
2019-02-24 11:27:17 +0000

    Cleaned yp file

--- ADCFir.m
+++ ADCFir.m
@@ -1,66 +1,56 @@
+%Globals:
 global data;
 data = niftiread("nonPregnant1708_2101.nii.gz");
-global protocol_21;
 realSignals = data;
+trueSignal = squeeze(data(101, 91, 26, :));
+
+global protocol_21;
 protocol_21 = load('protocol_21.txt');
 bValues = protocol_21(:,4);
-trueSignal = squeeze(data(101, 91, 26, :));
-global best_ADC;
 
-%testSignal = ADCFit(ADCGuess, s0Guess, bValues);
-%optimise(testSignal, trueSignal);
+global model_to_fit;
+% 0 - ADC Model
+% 1 - Ball-Ball Model
+% 2 - Stick Model
+%global best_ADC;
+
+
+%Fit Models
 lb = [0 0];    
 x0 = [0.01; 0.01];
     
+model_to_fit = 0;
 ADCopt = fmincon(@cominedOptimise, x0, [], [], [], [], lb, []);
+
 disp(ADCopt);
-disp(best_ADC);
-plot_ADC(best_ADC);
-
-%dont worry about this
-function testSignal = ADCFit(ADCGuess, s0Guess, bValues)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    for i = 1:size(bValues)
-        testSignal(i) = s0Guess*exp(-1*bValues(i)*ADCGuess);
-    end
-    disp(testSignal);
-end
-
-%dont worry about this
-function sum = optimise(testSignal, trueSignal)
-    sum = 0;
-    for i = 1:size(trueSignal)
-        sum = sum + (testSignal(i)-trueSignal(i))^2;
-    end
-    disp(sum);
-end
+%disp(best_ADC);
+plot_ADC(ADCopt);
 
 %this is the function that gets minimised
 function sum = cominedOptimise(ADCGuess)
-   % data = niftiread('nonPregnant1708_2101.nii.gz');
-   global data;
+    global data;
     data = double(data);
+    
     global protocol_21;
-   % protocol_21 = load('protocol_21.txt');
     protocol_21 = double(protocol_21);
     trueSignal = squeeze(data(101, 91, 26, :));
-    %s0Guess = 1/mean(trueSignal);
-    %trueSignal = trueSignal /trueSignal(1);
+    
     global bValues;
     bValues = protocol_21(:,4);
+    
+    %Produce Test Signals
     for i = 1:size(bValues)
        % testSignal(i) = s0Guess*exp(-1*bValues(i)*ADCGuess);
        testSignal(i) = ADCGuess(2,1)*exp(-1*bValues(i)*ADCGuess(1,1));
     end
     
+    %Find Mean Squared Error (What is minimised0
     sum = 0;
     for i = 1:size(trueSignal)
         sum = sum + (testSignal(i)-trueSignal(i))^2;
     end
     sum = double(sum);
-    global best_ADC;
-    best_ADC = ADCGuess;
+    
 end
 
 %function that replots the values
